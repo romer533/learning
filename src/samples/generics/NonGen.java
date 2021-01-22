d708346c6c8df84b31414f60b9814a4a36ca89ac
RomanGoltsov <romer533@mail.ru>
2021-01-22 15:51:02 +0600

    NonGenDemo
    Demonstrate a non-generic class

---
+++ src/samples/generics/NonGen.java
@@ -0,0 +1,20 @@
+package samples.generics;
+
+// Класс Non Gen - функциональный эквивалент класса Gen без обобщений
+public class NonGen {
+
+    Object ob;
+
+    NonGen(Object ob) {
+        this.ob = ob;
+    }
+
+    Object getOb() {
+        return ob;
+    }
+
+    void showType() {
+        System.out.println("Объект ob относится к типу " + ob.getClass().getName());
+    }
+
+}
